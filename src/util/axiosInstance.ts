b6540b2bd6278b25d824e8257fb42ed2ba457ea1
Harry <yjkang1223@naver.com>
2024-05-10 17:26:55 +0900

    🐛 Fix localStorage unimport issue
    fix localStorage issue

--- src/util/axiosInstance.ts
+++ src/util/axiosInstance.ts
@@ -3,9 +3,10 @@ import axios, { AxiosInstance } from "axios";
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: "https://bootcamp-api.codeit.kr/api/",
 });
-
-const token = localStorage.getItem("accessToken");
-axiosInstance.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+if (typeof window !== "undefined") {
+  const token = localStorage.getItem("accessToken");
+  axiosInstance.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  });
+}
